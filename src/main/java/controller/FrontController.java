78f873d6c56be04446cb898f6fce534f30dd3789
Sur0n <skyzephyr333@gmail.com>
2025-10-06 02:30:30 +0900

    fix : dynamic web bug

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("*.view")
+@WebServlet("/*.view")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
