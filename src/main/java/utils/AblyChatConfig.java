6d3600f2afb37a937c72213c84f254ec98c6f3ca
Sur0n <skyzephyr333@gmail.com>
2025-10-17 12:00:55 +0900

    feat : add annotation

--- src/main/java/utils/AblyChatConfig.java
+++ src/main/java/utils/AblyChatConfig.java
@@ -1,3 +1,4 @@
+//AblyChatConfig.java
 package utils;
 
 import java.util.Optional;
@@ -11,15 +12,15 @@ public class AblyChatConfig {
     /**
      * ServletContext에서 Firebase Properties 객체를 가져옵니다.
      */
-    public static Optional<Properties> getAblyConfig(ServletContext ctx) {
-        Object config = ctx.getAttribute(ABLY_ATTRIBUTE);
-        
-        System.out.println("GetAblyConfig 의 config 자료= " + config);
-        if (config instanceof Properties) {
-            return Optional.of((Properties) config);
-        }
-        
-        System.err.println("ably 설정(Properties)을 ServletContext에서 찾을 수 없습니다.");
-        return Optional.empty();
-    }
+	public static Optional<Properties> getAblyConfig(ServletContext ctx) {
+	    Object config = ctx.getAttribute(ABLY_ATTRIBUTE);
+	    
+	    System.out.println("GetAblyConfig 의 config 자료= " + config);
+	    if (config instanceof Properties) {
+	        return Optional.of((Properties) config);
+	    }
+	    
+	    System.err.println("ably 설정(Properties)을 ServletContext에서 찾을 수 없습니다.");
+	    return Optional.empty();
+	}
 }
